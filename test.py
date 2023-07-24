ec0047c11a3171bbbdc42c4cad745ae4e382898d
Ángel René Herrera Calzada <46908660+angelrene45@users.noreply.github.com>
2023-07-24 18:14:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -13,3 +13,8 @@ and name = $$var2
 """
 found_strings = get_double_dollar_strings(text)
 print(found_strings)
+
+
+def get_sql(text):
+    sql_matches = re.findall(r"```sql\n(.*?)\n```", text, re.DOTALL)
+    return sql_matches[-1] if sql_matches else None
